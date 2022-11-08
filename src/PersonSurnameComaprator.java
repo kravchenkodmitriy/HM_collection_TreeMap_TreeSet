853a3663b9686a662b5819cc2175c5bcfcab8722
Dmitriy <kravchenkodmitriy38@gmail.com>
2022-11-08 22:39:59 +0300

    Bug fix

--- src/PersonSurnameComaprator.java
+++ src/PersonSurnameComaprator.java
@@ -14,22 +14,11 @@ public class PersonSurnameComaprator implements Comparator<Person> {
 
     @Override
     public int compare(Person o1, Person o2) {
-        int comparator = Integer.compare(o2.getSurnname().length(), o1.getSurnname().length());
-        if (comparator != 0) {
-            if (o1.getSurnname().length() > getAmountWordSurname() && o2.getSurnname().length() > getAmountWordSurname()) {
-                return Integer.compare(o2.getAge(),o1.getAge());
-            } else {
-                return comparator;
-            }
-        } else {
-
-            return Integer.compare(o2.getAge(), o1.getAge());
+        int lengthSurname1 = o1.getSurnname().split(" ").length;
+        int lengthSurname2 = o2.getSurnname().split(" ").length;
+        if (Math.min(lengthSurname1, amountWordSurname) != Math.min(lengthSurname2, amountWordSurname)) {
+            return Integer.compare(lengthSurname1, lengthSurname2);
         }
-//        int lengthSurname1 = o1.getSurnname().split(" ").length;
-//        int lengthSurname2 = o2.getSurnname().split(" ").length;
-//        if ((lengthSurname1 >= amountWordSurname) != (lengthSurname2 >= amountWordSurname)) {
-//            return Integer.compare(lengthSurname1, lengthSurname2);
-//        }
-//        return Integer.compare(o1.getAge(), o2.getAge());
+        return Integer.compare(o1.getAge(), o2.getAge());
     }
 }
